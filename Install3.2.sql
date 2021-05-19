8ab408de3c0ca08c8ab2f207d5c8dec92f6922a8
brittainmark <gitbrit@inner-light.co.uk>
2021-05-19 08:01:27 +0100

    Update Install3.2.sql
    Modify set variable := to INTO variable

--- Install3.2.sql
+++ Install3.2.sql
@@ -3,84 +3,84 @@
 # 
 #  Create or find the Delivery option
 # 
-SELECT @options_id:=ifnull(max(products_options_id)+1,1) FROM products_options;
+SELECT ifnull(max(products_options_id)+1,1) INTO @options_id FROM products_options;
 
-SELECT @options_id := ifnull(products_options_id, @options_id) FROM products_options WHERE products_options_name='Minimum Delivery Method:' ;
+SELECT ifnull(products_options_id, @options_id) INTO @options_id FROM products_options WHERE products_options_name='Minimum Delivery Method:' ;
 
 REPLACE INTO products_options (products_options_id, language_id, products_options_name, products_options_sort_order, products_options_type, products_options_length, products_options_comment, products_options_size, products_options_images_per_row, products_options_images_style, products_options_rows) VALUES (@options_id, 1, 'Minimum Delivery Method:', 0, 5, 32, '', 32, 0, 0, 0);
 
-SELECT @options_id := products_options_id FROM products_options WHERE products_options_name='Minimum Delivery Method:' ;
+SELECT products_options_id INTO @options_id FROM products_options WHERE products_options_name='Minimum Delivery Method:' ;
 
 # 
 #  Set up the option values
 # 
 #  Large Letter
 # 
-SELECT @LargeLetter_id := ifnull(max(products_options_values_id)+1,1) FROM products_options_values;
+SELECT ifnull(max(products_options_values_id)+1,1) INTO @LargeLetter_id FROM products_options_values;
 
-SELECT @LargeLetter_id := ifnull(products_options_values_id, @LargeLetter_id) FROM products_options_values WHERE products_options_values_name='Large Letter' ;
+SELECT ifnull(products_options_values_id, @LargeLetter_id) INTO @LargeLetter_id FROM products_options_values WHERE products_options_values_name='Large Letter' ;
 
 REPLACE INTO products_options_values (products_options_values_id, language_id, products_options_values_name, products_options_values_sort_order) VALUES (@LargeLetter_id, 1, 'Large Letter', 10);
 
-SELECT @LargeLetter_id := products_options_values_id FROM products_options_values WHERE products_options_values_name='Large Letter' ;
+SELECT products_options_values_id INTO @LargeLetter_id FROM products_options_values WHERE products_options_values_name='Large Letter' ;
 
 # 
 #  Small Parcel
 # 
-SELECT @SmallParcel_id := ifnull(max(products_options_values_id)+1,1) FROM products_options_values;
+SELECT ifnull(max(products_options_values_id)+1,1) INTO @SmallParcel_id FROM products_options_values;
 
-SELECT @SmallParcel_id := ifnull(products_options_values_id, @SmallParcel_id) FROM products_options_values WHERE products_options_values_name='Small Parcel' ;
+SELECT ifnull(products_options_values_id, @SmallParcel_id) INTO @SmallParcel_id FROM products_options_values WHERE products_options_values_name='Small Parcel' ;
 
 REPLACE INTO products_options_values (products_options_values_id, language_id, products_options_values_name, products_options_values_sort_order) VALUES (@SmallParcel_id, 1, 'Small Parcel', 20);
 
 
-SELECT @SmallParcel_id := products_options_values_id FROM products_options_values WHERE products_options_values_name='Small Parcel' ;
+SELECT products_options_values_id INTO @SmallParcel_id FROM products_options_values WHERE products_options_values_name='Small Parcel' ;
 
 # 
 #  Medium Parcel
 # 
-SELECT @MediumParcel_id := ifnull(max(products_options_values_id)+1,1) FROM products_options_values;
+SELECT ifnull(max(products_options_values_id)+1,1) INTO @MediumParcel_id FROM products_options_values;
 
-SELECT @MediumParcel_id := ifnull(products_options_values_id, @MediumParcel_id) FROM products_options_values WHERE products_options_values_name='Medium Parcel' ;
+SELECT ifnull(products_options_values_id, @MediumParcel_id) INTO @MediumParcel_id FROM products_options_values WHERE products_options_values_name='Medium Parcel' ;
 
 REPLACE INTO products_options_values (products_options_values_id, language_id, products_options_values_name, products_options_values_sort_order) VALUES (@MediumParcel_id, 1, 'Medium Parcel', 30);
 
 
-SELECT @MediumParcel_id := products_options_values_id FROM products_options_values WHERE products_options_values_name='Medium Parcel' ;
+SELECT products_options_values_id INTO @MediumParcel_id FROM products_options_values WHERE products_options_values_name='Medium Parcel' ;
 
 # 
 #  Parcel Force
 # 
-SELECT @ParcelForce_id := ifnull(max(products_options_values_id)+1,1) FROM products_options_values;
+SELECT ifnull(max(products_options_values_id)+1,1) INTO @ParcelForce_id FROM products_options_values;
 
-SELECT @ParcelForce_id := ifnull(products_options_values_id, @ParcelForce_id) FROM products_options_values WHERE products_options_values_name='Special Delivary or Parcel Force' ;
+SELECT ifnull(products_options_values_id, @ParcelForce_id) INTO @ParcelForce_id FROM products_options_values WHERE products_options_values_name='Special Delivary or Parcel Force' ;
 
 REPLACE INTO products_options_values (products_options_values_id, language_id, products_options_values_name, products_options_values_sort_order) VALUES (@ParcelForce_id, 1, 'Special Delivary or Parcel Force', 40);
 
 
-SELECT @ParcelForce_id := products_options_values_id FROM products_options_values WHERE products_options_values_name='Special Delivary or Parcel Force' ;
+SELECT products_options_values_id INTO @ParcelForce_id FROM products_options_values WHERE products_options_values_name='Special Delivary or Parcel Force' ;
 
 # 
 #  Link the option and option values.
 # 
-SELECT @Pov2po_id := ifnull(max(products_options_values_to_products_options_id)+1,1) FROM products_options_values_to_products_options;
+SELECT ifnull(max(products_options_values_to_products_options_id)+1,1) INTO @Pov2po_id FROM products_options_values_to_products_options;
 
-SELECT @Pov2po_id := ifnull(products_options_values_to_products_options_id, @Pov2po_id) FROM products_options_values_to_products_options WHERE products_options_id= @options_id AND products_options_values_id = @LargeLetter_id;
+SELECT ifnull(products_options_values_to_products_options_id, @Pov2po_id) INTO @Pov2po_id FROM products_options_values_to_products_options WHERE products_options_id= @options_id AND products_options_values_id = @LargeLetter_id;
 REPLACE INTO products_options_values_to_products_options (products_options_values_to_products_options_id, products_options_id, products_options_values_id) VALUES (@Pov2po_id, @options_id, @LargeLetter_id);
 
-SELECT @Pov2po_id := ifnull(max(products_options_values_to_products_options_id)+1,1) FROM products_options_values_to_products_options;
+SELECT ifnull(max(products_options_values_to_products_options_id)+1,1) INTO @Pov2po_id FROM products_options_values_to_products_options;
 
-SELECT @Pov2po_id := ifnull(products_options_values_to_products_options_id, @Pov2po_id) FROM products_options_values_to_products_options WHERE products_options_id= @options_id AND products_options_values_id = @SmallParcel_id;
+SELECT ifnull(products_options_values_to_products_options_id, @Pov2po_id) INTO @Pov2po_id FROM products_options_values_to_products_options WHERE products_options_id= @options_id AND products_options_values_id = @SmallParcel_id;
 REPLACE INTO products_options_values_to_products_options (products_options_values_to_products_options_id, products_options_id, products_options_values_id) VALUES (@Pov2po_id, @options_id, @SmallParcel_id);
 
-SELECT @Pov2po_id := ifnull(max(products_options_values_to_products_options_id)+1,1) FROM products_options_values_to_products_options;
+SELECT ifnull(max(products_options_values_to_products_options_id)+1,1) INTO @Pov2po_id FROM products_options_values_to_products_options;
 
-SELECT @Pov2po_id := ifnull(products_options_values_to_products_options_id, @Pov2po_id) FROM products_options_values_to_products_options WHERE products_options_id= @options_id AND products_options_values_id = @MediumParcel_id;
+SELECT ifnull(products_options_values_to_products_options_id, @Pov2po_id) INTO @Pov2po_id FROM products_options_values_to_products_options WHERE products_options_id= @options_id AND products_options_values_id = @MediumParcel_id;
 REPLACE INTO products_options_values_to_products_options (products_options_values_to_products_options_id, products_options_id, products_options_values_id) VALUES (@Pov2po_id, @options_id, @MediumParcel_id);
 
-SELECT @Pov2po_id := ifnull(max(products_options_values_to_products_options_id)+1,1) FROM products_options_values_to_products_options;
+SELECT ifnull(max(products_options_values_to_products_options_id)+1,1) INTO @Pov2po_id FROM products_options_values_to_products_options;
 
-SELECT @Pov2po_id := ifnull(products_options_values_to_products_options_id, @Pov2po_id) FROM products_options_values_to_products_options WHERE products_options_id= @options_id AND products_options_values_id = @ParcelForce_id;
+SELECT ifnull(products_options_values_to_products_options_id, @Pov2po_id) INTO @Pov2po_id FROM products_options_values_to_products_options WHERE products_options_id= @options_id AND products_options_values_id = @ParcelForce_id;
 REPLACE INTO products_options_values_to_products_options (products_options_values_to_products_options_id, products_options_id, products_options_values_id) VALUES (@Pov2po_id, @options_id, @ParcelForce_id);
 
 #  
